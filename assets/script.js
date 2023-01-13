b82113446e956ac5fa45ec0e8d1b41f5ee2bd7d9
Crypto_Vdim <vadimrace@gmail.com>
2023-01-13 11:05:30 -0500

    created pseudocode for weather and map api calls

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,14 @@
+//Get API Keys for Maps and Weather
+// read API data for WX and Map and display on each card based on location
+
+// create getThisWeather() to pull weather data
+//create displayWeather() to display weather on the card
+//create function and call function with console log at the minimum
+//ideally link function to the card
+
+//create getThisMap() to pull map data
+//create displayMap() to display map data
+//create function and call function with console log at the minimum
+//ideally link function to the card
+
 console.log("pikachu")
