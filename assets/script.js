2f54e89a4bf690c9d9ae2f4b2df4d30354786edb
Junho Kim <junhokim@Junhos-MBP.attlocal.net>
2023-01-13 14:07:35 -0500

    added the map

--- assets/script.js
+++ assets/script.js
@@ -12,3 +12,29 @@
 //ideally link function to the card
 
 console.log("pikachu")
+
+let map;
+let map2;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map1"), {
+    center: { lat: 35.46708705310899, lng: -83.92043416136879 },
+    zoom: 8,
+  });
+  new google.maps.Marker({
+    position: { lat: 35.46708705310899, lng: -83.92043416136879 },
+    map: map,
+    label:"A",
+    title: "Tail of the Dragon",
+  })
+    map2 = new google.maps.Map(document.getElementById("map2"), {
+    center: { lat: 34.74042959585908, lng: -83.93676926931562 },
+    zoom: 8,
+  }); 
+  new google.maps.Marker({
+    position: { lat: 34.74042959585908, lng: -83.93676926931562 },
+    map: map2,
+    label:"A",
+    title: "Blood Mountain",
+  })
+}
