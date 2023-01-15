ec02c268ffa5a9aa2f120668c3512643849e15ca
Crypto_Vdim <vadimrace@gmail.com>
2023-01-14 22:37:17 -0500

    completed display function for 3 day weather forecast, fixed html, added dayjs

--- assets/script.js
+++ assets/script.js
@@ -15,25 +15,51 @@ function getWeather(lat, lon, cardNumber) {
     lat = lat;
     lon = lon;
     units = 'imperial';
-    var temp;
-    var wind;
-    var wxIcon;
-    var humidity;
-    var time;
+    var temp1;
+    var wind1;
+    var wxIcon1;
+    var humidity1;
+    var when1;
 
-    var requestUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${units}&appid=${apiKey}`;
+    var temp2;
+    var wind2;
+    var wxIcon2;
+    var humidity2;
+    var when2;
+
+    var temp3;
+    var wind3;
+    var wxIcon3;
+    var humidity3;
+    var when3;
+
+    var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=${units}&appid=${apiKey}`;
 
     fetch(requestUrl)
         .then(function (response) {
           if(response.ok){
             response.json().then(function(data){
-              //icon code, temp, wind, humidity
-              temp = data.main.temp;
-              wind = data.wind.speed;
-              wxIcon = data.weather[0].icon;
-              humidity = data.main.humidity;
-              time = data.dt;
-              displayWeather(wxIcon, temp, wind, humidity, time, cardNumber);
+              console.log(data);
+              //get new day data at 0(tomorrow), 8 (day+1), 16 (day+2),
+              //temp, wind, wxIcon, humidity, dtg
+              temp1 = data.list[0].main.temp;
+              wind1 = data.list[0].wind.speed;
+              wxIcon1 = data.list[0].weather[0].icon;
+              humidity1 = data.list[0].main.humidity;
+              when1 = data.list[0].dt_txt;
+
+              temp2 = data.list[8].main.temp;
+              wind2 = data.list[8].wind.speed;
+              wxIcon2 = data.list[8].weather[0].icon;
+              humidity2 = data.list[8].main.humidity;
+              when2 = data.list[8].dt_txt;
+              
+              temp3 = data.list[16].main.temp;
+              wind3 = data.list[16].wind.speed;
+              wxIcon3 = data.list[16].weather[0].icon;
+              humidity3 = data.list[16].main.humidity;
+              when3 = data.list[16].dt_txt;
+              displayWeather(wxIcon1, temp1, wind1, humidity1, when1, wxIcon2, temp2, wind2, humidity2, when2, wxIcon3, temp3, wind3, humidity3, when3, cardNumber);
             });
           } else{
             alert('Error: '+ response.statusText);
@@ -44,29 +70,60 @@ function getWeather(lat, lon, cardNumber) {
           alert('Unable to connect to openweathermap API');
         })
 }
-getWeather(35.4667, -83.9203, 1);
+getWeather(25.1963, -80.4134, 1);
+getWeather(34.9475, -101.68, 2);
+getWeather(30.3895, -89.0003, 3);
 
 
 // function displayWeatherIcon(){
 
 // }
+function displayDayWeather(functIcon, funcTemp, funcWind, funcHumidity, funcWhen){
+  var mark = funcWhen;
+  wxDateP = document.createElement('p');
+  wxIconP = document.createElement('p');
+  wxTempP = document.createElement('p');
+  wxWindP = document.createElement('p');
+  wxHumP = document.createElement('p');
+  wxDateP.innerHTML = `${mark}`;
+  wxIconP.innerHTML = `${functIcon}`
+  wxTempP.innerHTML = `Temp: ${funcTemp} °F`;
+  wxWindP.innerHTML = `Wind: ${funcWind} MPH`;
+  wxHumP.innerHTML = `Humidity: ${funcHumidity} %`;
 
-function displayWeather(functIcon, funcTemp, funcWind, funcHumidity, funcTime, cardNumber){
-  var mark = dayjs.unix(funcTime).format('MMM D, YYYY'); 
+
+}
+
+function displayWeather(functIcon1, funcTemp1, funcWind1, funcHumidity1, funcWhen1, functIcon2, funcTemp2, funcWind2, funcHumidity2, funcWhen2, functIcon3, funcTemp3, funcWind3, funcHumidity3, funcWhen3, cardNumber){
   wxEl = document.querySelector(`#wx-${cardNumber}`);
-    wxDateP = document.createElement('p');
-    wxTempP = document.createElement('p');
-    wxWindP = document.createElement('p');
-    wxHumP = document.createElement('p');
-    wxDateP.innerHTML = `${mark}`;
-    wxTempP.innerHTML = `Temp: ${funcTemp} °F`;
-    wxWindP.innerHTML = `Wind: ${funcWind} MPH`;
-    wxHumP.innerHTML = `Humidity: ${funcHumidity} %`;
-    //console.log(wxDateP);
-    wxEl.appendChild(wxDateP);
-    wxEl.appendChild(wxTempP);
-    wxEl.appendChild(wxWindP);
-    wxEl.appendChild(wxHumP);
+  wxDay1Div = document.createElement('div');
+  wxDay2Div = document.createElement('div');
+  wxDay3Div = document.createElement('div');
+  wxDay1Div.classList.add('col');
+  wxDay2Div.classList.add('col');
+  wxDay3Div.classList.add('col');
+  wxEl.appendChild(wxDay1Div);
+  displayDayWeather(functIcon1, funcTemp1, funcWind1, funcHumidity1, funcWhen1);
+  wxDay1Div.appendChild(wxDateP);
+  wxDay1Div.appendChild(wxIconP);
+  wxDay1Div.appendChild(wxTempP);
+  wxDay1Div.appendChild(wxWindP);
+  wxDay1Div.appendChild(wxHumP);
+  wxEl.appendChild(wxDay2Div);
+  displayDayWeather(functIcon2, funcTemp2, funcWind2, funcHumidity2, funcWhen2);
+  wxDay2Div.appendChild(wxDateP);
+  wxDay2Div.appendChild(wxIconP);
+  wxDay2Div.appendChild(wxTempP);
+  wxDay2Div.appendChild(wxWindP);
+  wxDay2Div.appendChild(wxHumP);
+  wxEl.appendChild(wxDay3Div);
+  displayDayWeather(functIcon3, funcTemp3, funcWind3, funcHumidity3, funcWhen3);
+  wxDay3Div.appendChild(wxDateP);
+  wxDay3Div.appendChild(wxIconP);
+  wxDay3Div.appendChild(wxTempP);
+  wxDay3Div.appendChild(wxWindP);
+  wxDay3Div.appendChild(wxHumP);
+
 };
 
 
